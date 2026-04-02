2308d8617fa27efd969f2b4e5014eb84e26adead
Abhishek Kumar <1hk23is003@hkbk.edu.in>
2026-04-03 00:22:38 +0530

    fix cors isseu

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb+srv://docols:Ankit0733@cluster0.eh7wg0u.mongodb.net/golfDB?retryWrites=true&w=majority"
+
+
+console.log("ENV VALUE:", MONGO_URI);
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB connected ✅"))
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-import "./db.js"
+import dotenv from "dotenv";
+
+// 👇 VERY IMPORTANT (force load .env correctly)
+dotenv.config({ path: "./.env" });
+
+import "./db.js";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import authRoutes from "./routes/authRoutes.js";
 import scoreRoutes from "./routes/scoreRoutes.js";
 import drawRoutes from "./routes/drawRoutes.js";
 
-dotenv.config();
-
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -22,8 +29,6 @@ app.use("/api", authRoutes);
 app.use("/api", scoreRoutes);
 app.use("/api", drawRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
