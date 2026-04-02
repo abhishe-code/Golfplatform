7a7f0e25400d410793d26f6f29c1d2cfa482ba69
Abhishek Kumar <1hk23is003@hkbk.edu.in>
2026-04-02 22:21:59 +0530

    fixed mongo env

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const MONOGO_URL="mongodb+srv://docols:Ankit0733@cluster0.eh7wg0u.mongodb.net/?appName=Cluster0"
-mongoose.connect(MONOGO_URL)
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB connected ✅"))
   .catch((err) => console.log("DB Error:", err));
