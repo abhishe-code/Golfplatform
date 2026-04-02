32300c870e6f003e4c298c3196f4585f37946d2f
Abhishek Kumar <1hk23is003@hkbk.edu.in>
2026-04-02 22:06:30 +0530

    fix mongo atlas connection

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 const MONOGO_URL="mongodb+srv://docols:Ankit0733@cluster0.eh7wg0u.mongodb.net/?appName=Cluster0"
-mongoose.connect("mongodb://127.0.0.1:27017/golfDB")
+mongoose.connect(MONOGO_URL)
   .then(() => console.log("MongoDB connected ✅"))
   .catch((err) => console.log("DB Error:", err));
